81393a180954f8e7bb7dda4341775862b12a344c
Huy Dang <huy_dang@datahouse.com>
2022-07-10 16:00:21 +0700

    adding Story Template instead of export multiple component

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import Button, { ButtonProps } from '../src/Button';
+import { action } from '@storybook/addon-actions';
 
 const meta: Meta = {
   title: 'Button',
   component: Button,
+  argTypes: {
+    onClick: { action: 'clicked' },
+    children: {
+      defaultValue: 'Default Text',
+      description: 'Primary Button',
+    },
+  },
 };
 
 export default meta;
 
-export const Default = () => <Button variant="primary">Click Me</Button>;
+const Template: Story<ButtonProps> = args => <Button {...args} />;
 
-export const Secondary = () => <Button variant="secondary">Click Me</Button>;
+//create clone of Template function
+export const Default = Template.bind({}); //() => <Button variant="primary">Click Me</Button>;
+
+export const Secondary = Template.bind({}); //() => <Button variant="secondary">Click Me</Button>;
+
+//declare args of Secondary here
+Secondary.args = {
+  variant: 'secondary',
+  children: 'Secondary Button',
+  onClick: action('Secondary clicked!'),
+} as ButtonProps;
